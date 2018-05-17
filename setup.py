9deda5c3ae32fcf5c3a3e90a4f5adc727e870969
Andrii Dubytskyi <andrii.dubytskyi@gmail.com>
2018-05-16 19:10:14 -0500

    Changes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 import sys
 
 setup(name='python_ikr_client',
-      version='1.0.2',
+      version='1.0.3',
       description='',
       url='https://github.com/andrii-dubytskyi/python_ikr_client.git',
       download_url = 'https://github.com/andrii-dubytskyi/python_ikr_client/archive/master.zip',
